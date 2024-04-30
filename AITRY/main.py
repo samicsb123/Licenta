cf43bce403e33b98cfdbac05151ea55eb71c0c97
Sami <sami_radu83@yahoo.com>
2024-04-30 14:07:23 +0200

    am rezolvat partea de access home fara cont

--- AITRY/main.py
+++ AITRY/main.py
@@ -74,11 +74,11 @@ def signup():
 
 @app.route('/home')
 @login_required
-# FA AICI POST METHOD 
+# FA AICI POST METHOD ( cred ca asta e problema ca ma lasa sa accesez pag fara cont)
 def home():
     return render_template('home.html')
 
-@app.route('/logout')
+@app.route('/logout', methods=['GET','POST'])
 @login_required
 def logout():
     session.pop('logged_in', None)
